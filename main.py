edc012b880253c64d81ab1620397bcb17642c336
therrera2397 <therrera2397@gmail.com>
2025-09-03 23:30:56 +0000

    first start solution

--- main.py
+++ main.py
@@ -6,3 +6,9 @@ print(mean_var_std.calculate([0,1,2,3,4,5,6,7,8]))
 
 # Run unit tests automatically
 main(module='test_module', exit=False)
+from mean_var_std import calculate
+
+example = [0,1,2,3,4,5,6,7,8]
+result = calculate(example)
+print(result)
+
--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,23 @@
 import numpy as np
 
-def calculate(list):
+def calculate(numbers):
+    if len(numbers) != 9:
+        raise ValueError("List must contain nine numbers.")
 
+    arr = np.array(numbers).reshape(3, 3)
 
+    def collect(func):
+        return [func(arr, axis=0).tolist(),
+                func(arr, axis=1).tolist(),
+                func(arr).item()]
 
+    stats = {
+        'mean': collect(np.mean),
+        'variance': collect(np.var),
+        'standard deviation': collect(np.std),
+        'max': collect(np.max),
+        'min': collect(np.min),
+        'sum': collect(np.sum)
+    }
 
-    return calculations
+    return stats
